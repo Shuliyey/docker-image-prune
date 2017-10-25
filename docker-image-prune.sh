24b9b5940ea2ef264462d09309414fb1cb14e665
Zeyu Ye <z.ye@base2services.com>
2017-10-25 16:01:07 +1100

    update docker-image-prune.sh format

--- docker-image-prune.sh
+++ docker-image-prune.sh
@@ -4,6 +4,7 @@ lines=$(echo "$images" | awk '{print $2}' | grep -n "<none>" | cut -d: -f1 | sed
 lines=`echo $lines`;
 lines=${lines// /;};
 image_ids=$(echo "$images" | awk '{print $3}' | sed -n "$lines");
+
 for id in ${image_ids[@]};
 do
   exited_containers=$(docker ps --filter status=exited --filter ancestor=${id} -q);
@@ -11,6 +12,7 @@ do
   if [[ -n "$(echo ${exited_containers})" ]]
     docker rm ${exited_containers}
   fi
+
   if [[ ! -n "$(echo ${running_containers})" ]]; then
     docker rmi ${id};
   fi;
