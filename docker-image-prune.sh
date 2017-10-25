98298cd65f36dc57bbe5dca86e04fefd6b5ff13e
Zeyu Ye <z.ye@base2services.com>
2017-10-25 16:05:09 +1100

    update docker-image-prune.sh, remove ;

--- docker-image-prune.sh
+++ docker-image-prune.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-images=`docker images`;
-lines=$(echo "$images" | awk '{print $2}' | grep -n "<none>" | cut -d: -f1 | sed 's/$/p/g');
-lines=`echo $lines`;
-lines=${lines// /;};
-image_ids=$(echo "$images" | awk '{print $3}' | sed -n "$lines");
+images=`docker images`
+lines=$(echo "$images" | awk '{print $2}' | grep -n "<none>" | cut -d: -f1 | sed 's/$/p/g')
+lines=`echo $lines`
+lines=${lines// /;}
+image_ids=$(echo "$images" | awk '{print $3}' | sed -n "$lines")
 
 for id in ${image_ids[@]};
 do
-  exited_containers=$(docker ps --filter status=exited --filter ancestor=${id} -q);
-  running_containers=$(docker ps --filter status=running --filter ancestor=${id} -q);
+  exited_containers=$(docker ps --filter status=exited --filter ancestor=${id} -q)
+  running_containers=$(docker ps --filter status=running --filter ancestor=${id} -q)
   if [[ -n "$(echo ${exited_containers})" ]]; then
     docker rm ${exited_containers}
   fi
 
   if [[ ! -n "$(echo ${running_containers})" ]]; then
-    docker rmi ${id};
-  fi;
+    docker rmi ${id}
+  fi
 done
